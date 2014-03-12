571237a38517cdb555768e96299f12663da45554
rudametw <rudametkin@gmail.com>
2014-03-12 22:57:02 +0100

    Update site
    Former-commit-id: 4fde1bac01942495360b3b5ffe273d7fc319deb6

--- optimize-photos.sh
+++ optimize-photos.sh
@@ -27,25 +27,27 @@ for dir in $(find _site/ -type d); do
     if [ "$files" != "0" ]; then
 #        echo "*******************************"
         echo "Found jpg $files in `pwd`"
-        jpegoptim --max=90 --strip-all --preserve --totals *.jpg;
+#        jpegoptim --max=50 --strip-all --preserve --totals *.jpg;
+        jpegoptim --max=50 --strip-all --preserve --totals *.jpg;
     fi
 
     files=$(ls *.JPG 2> /dev/null | wc -l)
     if [ "$files" != "0" ]; then
         echo "*******************************"        
         echo "Found JPG $files in `pwd`"
-        jpegoptim --max=90 --strip-all --preserve --totals *.JPG
+        jpegoptim --max=50 --strip-all --preserve --totals *.JPG
     fi
 
-    files=$(ls *.png 2> /dev/null | wc -l)
-    if [ "$files" != "0" ]; then
-        echo "*******************************"        
-        echo "Found png $files in `pwd`"
-        #jpegoptim --max=90 --strip-all --preserve --totals *.png
-        #for i in *.png; do optipng -o5 -quiet -keep -preserve -log optipng.log "$i"; done
-#        for i in *.png; do optipng -o5 -preserve -clobber "$i"; done        
-        echo "Found pngs but not optimizing them"
-    fi
+#    files=$(ls *.png 2> /dev/null | wc -l)
+#    if [ "$files" != "0" ]; then
+#        echo "*******************************"        
+#        echo "Found png $files in `pwd`"
+#        #jpegoptim --max=90 --strip-all --preserve --totals *.png
+#        #for i in *.png; do optipng -o5 -quiet -keep -preserve -log optipng.log "$i"; done
+#        #for i in *.png; do optipng -o5 -preserve -clobber "$i"; done        
+#        for i in *.png; do optipng -o7 -preserve -clobber "$i"; done        
+#        echo "Found pngs but not optimizing them"
+#    fi
 
     echo
 
