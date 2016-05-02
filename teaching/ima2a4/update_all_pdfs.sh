1701fdaf8276159697efdaa7a08b7bf38df0d786
rudametw <rudametkin@gmail.com>
2016-05-02 13:02:23 +0200

    Update site

---
+++ teaching/ima2a4/update_all_pdfs.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+#'cp' -uav  ~/COURS/IMA3/Cours-Walter/PDFs/CM*.pdf  ~/COURS/IMA3/Cours-Walter/TP-Walter/TP*/*.pdf   ~/COURS/IMA3/Cours-Walter/TD-Walter/TD*.pdf .
+'cp' -uav  /home/wrudamet/COURS/IMA2A4/Cours-Walter/PDFs/*.pdf .
+
+'cp' -uav  /home/wrudamet/COURS/IMA2A4/TP-Walter/sujet1/sujet1.pdf .
+
+'cp' -uav  /home/wrudamet/COURS/IMA2A4/TP-Walter/sujets_tps_circuits/tp*.pdf  .
+
