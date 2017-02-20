fe0c1214bc71b2eb898812998cee38cb7ef25bf9
rudametw <rudametkin@gmail.com>
2017-02-20 18:08:43 +0100

    Update site

--- src/update_site.sh
+++ src/update_site.sh
@@ -1,1 +1,1 @@
-'cp' -uvarf _site/* ../ && git add ../ && git commit -m "Update site" && git push
+'cp' -uvarf _site/* ../ && git add ../ && echo "***** git status *****" && git status  && git commit -m "Update site" && git push
