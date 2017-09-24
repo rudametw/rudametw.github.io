809df069c5a4d4c77c03f37de00631bed411728d
rudametw <rudametkin@gmail.com>
2017-09-24 16:14:36 +0200

    Update site

--- src/update_site.sh
+++ src/update_site.sh
@@ -1,4 +1,5 @@
 #This builds a production site, makes sure urls are correctly set
-jekyll build
+#Doesn't work because photos are no longer properly built!!!
+#jekyll build
 
 'cp' -uvarf _site/* ../ && git add ../ && echo "***** git status *****" && git status  && git commit -m "Update site" && git push
