3af881277509253c390663cfaf1cf749cb77dfdb
rudametw <rudametkin@gmail.com>
2017-09-24 16:07:05 +0200

    Fixes problem with dev localhost urls being sent to production

--- src/update_site.sh
+++ src/update_site.sh
@@ -1,1 +1,4 @@
+#This builds a production site, makes sure urls are correctly set
+jekyll build
+
 'cp' -uvarf _site/* ../ && git add ../ && echo "***** git status *****" && git status  && git commit -m "Update site" && git push
