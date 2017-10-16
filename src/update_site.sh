6320ffc394d6a234cb1d0c922136ba5660ccf7cd
rudametw <rudametkin@gmail.com>
2017-10-16 18:26:10 +0200

    removed extraneous files

--- src/update_site.sh
+++ src/update_site.sh
@@ -61,6 +61,7 @@ sleep 1
 #'cp' -uvarf _site/* ../
 #Update site files, commit and push
 'cp' -uvarf _site/* ../ && git add ../ && echo "***** git status *****" && git status  && git commit -m "Update site" && git push
+rm ../debug.log
 
 sleep 3
 echo
