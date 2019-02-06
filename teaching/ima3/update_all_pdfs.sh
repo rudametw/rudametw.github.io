43fe3b94e1364c49692ea3b25a494d2fb362d552
rudametw <rudametkin@gmail.com>
2019-02-06 15:51:28 +0100

    Update site

--- teaching/ima3/update_all_pdfs.sh
+++ teaching/ima3/update_all_pdfs.sh
@@ -1,1 +1,1 @@
-'cp' -uav  ~/COURS/IMA3/Cours-Walter/PDFs/CM*.pdf  ~/COURS/IMA3/Cours-Walter/TP-Walter/TP*/*.pdf   ~/COURS/IMA3/Cours-Walter/TD-Walter/TD*.pdf .
+'cp' -uav  ~/COURS/IMA3/Cours-Walter/PDFs/CM*.pdf  ~/COURS/IMA3/Cours-Walter/TP/TP*/*.pdf   ~/COURS/IMA3/Cours-Walter/TD/TD*.pdf .
