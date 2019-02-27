f4115aa176381d0f6e761b9ddbd7bf61ec4c7596
rudametw <rudametkin@gmail.com>
2019-02-27 09:14:25 +0100

    Update site

--- src/update_jekyll.sh
+++ src/update_jekyll.sh
@@ -2,3 +2,8 @@ gem cleanup
 bundle update
 
 echo "***\n if any Errors... try C_INCLUDE_PATH=/usr/include/ImageMagick-6 gem install rmagick"
+
+echo Or maybe
+echo sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+echo sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
--- teaching/ima3/update_all_pdfs.sh
+++ teaching/ima3/update_all_pdfs.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
 #'cp' -uav --reflink=auto --sparse=always  ~/COURS/IMA3/Cours-Walter/PDFs/CM*.pdf  ~/COURS/IMA3/Cours-Walter/TP/TP*/*.pdf   ~/COURS/IMA3/Cours-Walter/TD/TD*.pdf .
+
+#Individual copies
 'cp' -uav --reflink=auto --sparse=always ~/COURS/IMA3/Cours-Walter/PDFs/CM*.pdf .
 'cp' -uav --reflink=auto --sparse=always ~/COURS/IMA3/Cours-Walter/TP/TP*/TP*.pdf .
 'cp' -uav --reflink=auto --sparse=always ~/COURS/IMA3/Cours-Walter/TD/TD*.pdf .
